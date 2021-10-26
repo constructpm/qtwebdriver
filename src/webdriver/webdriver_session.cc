139b05ae2d381dde52fe1c76124dd4cdc179ce9b
Ben Falconer <ben@falconers.me.uk>
2021-10-26 14:44:55 +0100

    Fix invalid iterator causing undefined behaviour

--- src/webdriver/webdriver_session.cc
+++ src/webdriver/webdriver_session.cc
@@ -501,13 +501,19 @@ void Session::UpdateViews(const std::set<ViewId>& views) {
     ViewsMap::iterator it;
     ViewId vi;
 
-    for (it = views_.begin(); it != views_.end(); ++it) {
+    for (it = views_.begin(); it != views_.end();) {
         vi = ViewId(it->first);
 		
         if (vi.is_valid() && 0 == views.count(vi)) {
+			it = views_.erase( it );
             // invalidate handle
             RemoveView(vi);
+			break;
         }
+		else
+		{
+			++it;
+		}
     }
 }
 
